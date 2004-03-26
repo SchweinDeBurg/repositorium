dff318ef6954b3aad391f3fac89d1aa57f20ff14
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2004-03-26 22:05:24 +0000

    Added CSortingListCtrl class - list control with built-in sorting support.

---
+++ trunk/AfxGadgets/Source/SortingListCtrl.h
@@ -0,0 +1,52 @@
+// AfxGadgets library.
+// Copyright (c) 2004 by Elijah Zarezky,
+// All rights reserved.
+
+// SortingListCtrl.h - interface of the CSortingListCtrl class
+
+#if !defined(__SortingListCtrl_h)
+#define __SortingListCtrl_h
+
+class CSortingListCtrl: public CListCtrl
+{
+	DECLARE_DYNAMIC(CSortingListCtrl)
+	DECLARE_MESSAGE_MAP()
+
+// construction/destruction
+protected:
+	CSortingListCtrl(void);
+	virtual ~CSortingListCtrl(void);
+
+// operations
+public:
+	enum SORT_ORDER { SORT_ASCENDING = 1, SORT_DESCENDING = -1 };
+	void SortItems(int iColumn, SORT_ORDER eOrder);
+
+// overridables
+protected:
+	virtual int CompareItems(int iItemLhs, int iItemRhs) = 0;
+
+// message map functions
+protected:
+	afx_msg void OnColumnClick(NMHDR* pHdr, LRESULT* pnResult);
+
+// attributes
+protected:
+	int m_iSortColumn;
+	int m_nSortOrder;		// SORT_ASCENDING or SORT_DESCENDING
+
+// implementation helpers
+private:
+	static int CALLBACK CompareProc(LPARAM iItemLhs, LPARAM iItemRhs, LPARAM nData);
+
+// diagnostic services
+#if defined(_DEBUG)
+public:
+	virtual void AssertValid(void) const;
+	virtual void Dump(CDumpContext& dumpCtx) const;
+#endif
+};
+
+#endif	// __SortingListCtrl_h
+
+// end of file
