e03273059c77d7e416c4d545dc006f0bcc0113f0
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2005-01-03 11:34:06 +0000

    Updated source code comments due to new year beginning.

--- AfxGadgets/Source/Win32Error.h
+++ AfxGadgets/Source/Win32Error.h
@@ -1,5 +1,5 @@
 // AfxGadgets library.
-// Copyright (c) 2003-2004 by Elijah Zarezky,
+// Copyright (c) 2003-2005 by Elijah Zarezky,
 // All rights reserved.
 // Portions copyright (c) 2001 by Ajit Jadhav.
 
