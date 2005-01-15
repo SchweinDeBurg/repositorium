bc24870703ef7cef676db19c68ee768846bb440c
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2005-01-15 22:56:34 +0000

    Added PugXML parser extensions and wrappers. Added CDialogXML class, which encapsulates XML-based dialogs.

--- trunk/AfxGadgets/Source/stdafx.h
+++ trunk/AfxGadgets/Source/stdafx.h
@@ -7,6 +7,13 @@
 #if !defined(__stdafx_h)
 #define __stdafx_h
 
+// unreferenced inline/local function has been removed
+#pragma warning(disable: 4514)
+// function not inlined
+#pragma warning(disable: 4710)
+// identifier was truncated in the debug information
+#pragma warning(disable: 4786)
+
 // use Win98/NT4 features
 #define _WIN32_WINNT 0x0400
 
@@ -18,6 +25,24 @@
 // RSDK headers
 #include <wincrypt.h>		// cryptographic API prototypes and definitions
 #include <oleauto.h>			// OLE automation support
+#include <shlobj.h>
+#include <shlwapi.h>			// light-weight utility APIs
+
+// STL headers
+#pragma warning(push, 3)
+#include <iostream>
+#include <memory>
+#pragma warning(pop)
+
+// for compatibility with Dinkumware STL
+#if !defined(_IOSTREAM_)
+#define _IOSTREAM_
+#endif
+
+#define _FSTREAM_
+
+// additional headers
+#include "../../CodeProject/Source/PugXML.h"
 
 #endif	// __stdafx_h
 
