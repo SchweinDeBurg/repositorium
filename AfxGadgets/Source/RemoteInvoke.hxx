fd1a29e93c61527c6531b6b4bea4a40148c50145
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2005-07-16 00:02:18 +0000

    Added CRemoteInvoke, CRemoteProc and CRemoteParam template classes - encapsulation of the CreateRemoteThread() API.

---
+++ AfxGadgets/Source/RemoteInvoke.hxx
@@ -0,0 +1,88 @@
+// AfxGadgets library.
+// Copyright (c) 2005 by Elijah Zarezky,
+// All rights reserved.
+
+// RemoteInvoke.hxx - interface and implementation of the CRemoteInvoke template class
+
+#if !defined(__RemoteInvoke_hxx)
+#define __RemoteInvoke_hxx
+
+#if defined(_MSC_VER) && (_MSC_VER > 1000)
+#pragma once
+#endif	// _MSC_VER
+
+#include "RemoteProc.hxx"
+
+// interface
+
+template <class _Data_t, DWORD(WINAPI* _Proc)(CRemoteParam<_Data_t>*)>
+class CRemoteInvoke
+{
+// construction/destruction
+public:
+	CRemoteInvoke(_Data_t& dataRef);
+
+// copying/assignment - disabled and thus not implemented
+private:
+	CRemoteInvoke(const CRemoteInvoke& rhs);
+	CRemoteInvoke& operator=(const CRemoteInvoke& rhs);
+
+// operations
+public:
+	BOOL operator()(HANDLE hProcess, DWORD dwTimeOut = INFINITE);
+	BOOL operator()(DWORD dwProcessID, DWORD dwTimeOut = INFINITE);
+
+// attributes
+private:
+	_Data_t& m_dataRef;
+};
+
+// implementation
+
+template <class _Data_t, DWORD(WINAPI* _Proc)(CRemoteParam<_Data_t>*)>
+inline CRemoteInvoke<_Data_t, _Proc>::CRemoteInvoke(_Data_t& dataRef):
+m_dataRef(dataRef)
+{
+}
+
+template <class _Data_t, DWORD(WINAPI* _Proc)(CRemoteParam<_Data_t>*)>
+BOOL CRemoteInvoke<_Data_t, _Proc>::operator()(HANDLE hProcess, DWORD dwTimeOut)
+{
+	DWORD dwThreadID;
+	BOOL fSuccess;
+
+	CRemoteParam<_Data_t> param(hProcess, m_dataRef);
+	CRemoteProc<_Data_t, _Proc> proc(hProcess);
+	HANDLE hThread = ::CreateRemoteThread(hProcess, NULL, 0, proc, param, 0, &dwThreadID);
+	switch (::WaitForSingleObject(hThread, dwTimeOut))
+	{
+	case WAIT_OBJECT_0:
+		fSuccess = param.GetRemoteData(m_dataRef);
+		break;
+	case WAIT_TIMEOUT:
+		// time-out interval expired
+		fSuccess = FALSE;
+		break;
+	case WAIT_ABANDONED:
+		// shouldn't be reached
+		fSuccess = FALSE;
+		break;
+	default:
+		fSuccess = FALSE;
+	}
+	return (fSuccess);
+}
+
+template <class _Data_t, DWORD(WINAPI* _Proc)(CRemoteParam<_Data_t>*)>
+inline BOOL CRemoteInvoke<_Data_t, _Proc>::operator()(DWORD dwProcessID, DWORD dwTimeOut)
+{
+	enum { fdwAccess = PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ };
+	HANDLE hProcess = ::OpenProcess(fdwAccess, FALSE, dwProcessID);
+	BOOL fSuccess = (*this)(hProcess, dwTimeOut);
+	::CloseHandle(hProcess);
+	return (fSuccess);
+}
+
+#endif	// __RemoteInvoke_hxx
+
+// end of file
