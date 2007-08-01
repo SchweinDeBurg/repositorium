09e85389a4a58cf9381be89b2a192e14e75d8418
elijah <elijah@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2007-08-01 17:03:57 +0000

    Dropped Visual Studio 6.0-7.0 support. Changed output directories for compiler/linker. Corrected build scripts.

--- makeall.cmd
+++ makeall.cmd
@@ -1,4 +1,5 @@
 @echo off
+
 call maked.cmd
 call makeud.cmd
 call maker.cmd
--- makeud.cmd
+++ makeud.cmd
@@ -1,14 +1,14 @@
 @echo off
+
 echo.
 echo Building libraries - "Win32 Unicode Debug"...
 echo.
-msdev.com Repository.dsw /MAKE "AfxGadgets - Win32 Unicode Debug" /REBUILD
+
+devenv.com Repository71.sln /rebuild "Unicode Debug" /project AfxGadgets /nologo
 echo.
-msdev.com Repository.dsw /MAKE "atlmfc71 - Win32 Unicode Debug" /REBUILD
+devenv.com Repository71.sln /rebuild "Unicode Debug" /project CodeProject /nologo
 echo.
-msdev.com Repository.dsw /MAKE "CodeProject - Win32 Unicode Debug" /REBUILD
+devenv.com Repository71.sln /rebuild "Unicode Debug" /project Naughter /nologo
 echo.
-msdev.com Repository.dsw /MAKE "Naughter - Win32 Unicode Debug" /REBUILD
-echo.
-msdev.com Repository.dsw /MAKE "ZipArchive - Win32 Unicode Debug" /REBUILD
+devenv.com Repository71.sln /rebuild "Unicode Debug" /project ZipArchive /nologo
 echo.
