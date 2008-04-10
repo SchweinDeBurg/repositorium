f0ed656f8e55def239b502da5f2a583f97648274
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2008-04-10 06:37:05 +0000

    Added author's "CRC" sample to the ClassLib library.

---
+++ trunk/Jan_van_den_Baard/ClassLib/Samples/CRC/Source/Resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CRC.rc
+//
+#define IDI_APP_ICON                    1
+#define IDD_CRC                         101
+#define IDC_FILE                        1000
+#define IDC_GEN32                       1001
+#define IDC_GEN16                       1002
+#define IDC_TABLE                       1003
+#define IDC_CRC                         1004
+#define IDC_SETCRC                      1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
