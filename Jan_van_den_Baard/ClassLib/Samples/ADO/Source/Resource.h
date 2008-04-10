62b9e0cbb23dc85995957ea22fdb8952d4680a85
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2008-04-10 05:52:25 +0000

    Simplified linkage with the ClassLib library. Added author's "ADO" sample to the ClassLib library.

---
+++ Jan_van_den_Baard/ClassLib/Samples/ADO/Source/Resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ADO.rc
+//
+#define IDI_APP_ICON                    1
+#define IDD_ADO                         101
+#define IDC_TABLES                      1001
+#define IDC_CONN                        1002
+#define IDC_CONTENT                     1003
+#define IDC_LABEL1                      1005
+#define IDC_LABEL2                      1006
+#define IDC_LABEL3                      1007
+#define IDC_OK                          1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
--- Jan_van_den_Baard/ClassLib/Source/all.h
+++ Jan_van_den_Baard/ClassLib/Source/all.h
@@ -1,11 +1,37 @@
-#ifndef _ALL_H_
-#define _ALL_H_
 //
 // all.h
 //
 // (C) Copyright 1999-2003 Jan van den Baard
 //     All Rights Reserved.
 //
+
+#ifndef _ALL_H_
+#define _ALL_H_
+
+#define OEMRESOURCE
+
+// CRT headers
+
+// _CRTDBG_MAP_ALLOC is not #define-d because we use our own implementation
+// of the new and delete operators (see application.cpp file)
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <tchar.h>
+#include <malloc.h>
+#include <crtdbg.h>
+#include <new.h>
+
+// PSDK headers
+
+#include <windows.h>
+#include <commctrl.h>
+#include <shlwapi.h>
+
+// ClassLib headers
 
 #include "standard.h"
 #include "application.h"
@@ -27,5 +53,16 @@
 #include "layout/all.h"
 #include "io/all.h"
 #include "shell/all.h"
+
+// import libraries
+
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "shell32.lib")
+#pragma comment(lib, "Gdi32.lib")
+#pragma comment(lib, "Advapi32.lib")
+#pragma comment(lib, "ole32.lib")
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(lib, "shlwapi.lib")
+#pragma comment(lib, "Comdlg32.lib")
 
 #endif // _ALL_H_
