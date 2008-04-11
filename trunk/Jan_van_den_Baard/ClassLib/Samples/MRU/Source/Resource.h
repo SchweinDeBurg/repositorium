5fdf821c8e579540ef87333f44923dcab42c8675
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2008-04-11 07:43:02 +0000

    Added author's "MRU" sample to the ClassLib library.

---
+++ trunk/Jan_van_den_Baard/ClassLib/Samples/MRU/Source/Resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MRU.rc
+//
+#define IDI_APP_ICON                    1
+#define IDD_MRU                         101
+#define ID_FILE_FUCK                    103
+#define ID_FILES                        105
+#define ID_FILES_SUBMENU                106
+#define IDB_BITMAP1                     107
+#define IDC_ADD                         1000
+#define IDC_REM                         1001
+#define IDC_SHOW                        1002
+#define IDC_SAVE                        1003
+#define IDC_OFFICE_XP                   1005
+#define IDC_ABOUT                       1006
+#define IDC_FILEICONS                   1007
+#define IDC_COMBO1                      1008
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
