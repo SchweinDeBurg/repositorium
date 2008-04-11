d57cd2c87e7dcffb529e8c697e4433fb8167693a
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2008-04-11 08:18:56 +0000

    Added author's "XPControls" sample to the ClassLib library.

---
+++ Jan_van_den_Baard/ClassLib/Samples/XPControls/Source/Resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by XPControls.rc
+//
+#define IDI_APP_ICON                    1
+#define IDD_XPC                         9
+#define IDC_EDIT_1                      1001
+#define IDC_EDIT_2                      1002
+#define IDC_COMBO_1                     1004
+#define IDC_COMBO_2                     1005
+#define IDC_COMBO_3                     1006
+#define IDC_COMBO_4                     1007
+#define IDC_OK                          1008
+#define IDC_OK_1                        1008
+#define IDC_OK_2                        1009
+#define IDC_CHECK1                      1010
+#define IDC_DISABLE                     1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
