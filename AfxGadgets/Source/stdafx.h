4014c87fd16796f2ee2ed6a65dd82c734958676b
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2008-07-07 06:42:44 +0000

    Improved source code formatting.

--- AfxGadgets/Source/stdafx.h
+++ AfxGadgets/Source/stdafx.h
@@ -23,6 +23,9 @@
 #if !defined(__stdafx_h)
 #define __stdafx_h
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+// unwanted warnings
+
 // unreferenced inline/local function has been removed
 #pragma warning(disable: 4514)
 // function not inlined
@@ -35,51 +38,61 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif   // _MSC_VER
 
-#if (_MSC_VER > 1310)
+//////////////////////////////////////////////////////////////////////////////////////////////
 // use WinXP/IE6 features
+
+#define WINVER 0x0501
+#define _WIN32_WINDOWS 0x0500
+#define _WIN32_IE 0x0600
 #define _WIN32_WINNT 0x0501
-#define _WIN32_IE 0x0601
-#else
-// use Win98/NT4 features
-#define _WIN32_WINNT 0x0400
-#endif   // _MSC_VER
 
-#if (_MSC_VER >= 1200)
-#define _WIN32_WINDOWS 0x0490
-#endif	// _MSC_VER
-
+//////////////////////////////////////////////////////////////////////////////////////////////
 // MFC headers
-#include <afxwin.h>			// MFC core and standard components
-#include <afxtempl.h>		// MFC collection template classes
-#include <afxcmn.h>			// MFC common control classes
+
+#include <afxwin.h>        // MFC core and standard components
+#include <afxtempl.h>      // MFC collection template classes
+#include <afxcmn.h>        // MFC common control classes
 #include <afxpriv.h>       // MFC private classes
 
 #include <../src/mfc/afximpl.h>
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // ATL headers
-#include <atlbase.h>			// for ATL::CComBSTR
-#include <ATLComTime.h>		// for ATL::CTime and ATL::COleDateTime
 
+#include <atlbase.h>       // for ATL::CComBSTR
+#include <ATLComTime.h>    // for ATL::CTime and ATL::COleDateTime
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 // RSDK headers
-#include <wincrypt.h>		// cryptographic API prototypes and definitions
-#include <oleauto.h>			// OLE automation support
-#include <shlobj.h>
-#include <shlwapi.h>			// light-weight utility APIs
 
+#include <wincrypt.h>      // cryptographic API prototypes and definitions
+#include <oleauto.h>       // OLE automation support
+#include <shlobj.h>
+#include <shlwapi.h>       // light-weight utility APIs
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 // STL headers
+
 #pragma warning(push, 3)
 #include <iostream>
 #include <memory>
 #pragma warning(pop)
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // for compatibility with Dinkumware STL
+
 #if !defined(_IOSTREAM_)
 #define _IOSTREAM_
 #endif	// _IOSTREAM_
 #define _FSTREAM_
 
+//////////////////////////////////////////////////////////////////////////////////////////////
 // additional headers
+
 #include "../../CodeProject/Source/PugXML.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// some tricks
 
 // force ISO/IEC 14882 conformance in for loop scope
 #if (_MSC_VER < 1300)
