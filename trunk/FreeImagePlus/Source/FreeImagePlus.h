84995ac439aa4844bf39fe748d6dcb43ae1c65c0
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2008-11-11 04:22:38 +0000

    Merged "FreeImage" library with SourceForge CVS.

--- trunk/FreeImagePlus/Source/FreeImagePlus.h
+++ trunk/FreeImagePlus/Source/FreeImagePlus.h
@@ -77,6 +77,9 @@
 class FIP_API fipObject
 {
 public:
+	/// Destructor
+	virtual ~fipObject(){};
+	
 	/**@name Information functions */
 	//@{
 	/// Returns TRUE if the object is allocated, FALSE otherwise
@@ -103,6 +106,8 @@ class FIP_API fipImage : public fipObject
 protected:
 	/// DIB data
 	FIBITMAP *_dib;
+	/// Original (or last saved) fif format if available, FIF_UNKNOWN otherwise
+	FREE_IMAGE_FORMAT _fif;
 	/// TRUE whenever the display need to be refreshed
 	mutable BOOL _bHasChanged;
 
@@ -119,7 +124,7 @@ public:
 	*/
 	fipImage(FREE_IMAGE_TYPE image_type = FIT_BITMAP, WORD width = 0, WORD height = 0, WORD bpp = 0);
 	/// Destructor
-	~fipImage();
+	virtual ~fipImage();
 	/**
 	Image allocator
 	@see FreeImage_AllocateT
@@ -190,8 +195,44 @@ public:
 	*/
 	BOOL crop(int left, int top, int right, int bottom);
 
+	//@}
+
+	/** @name File type identification
+	 */
+	//@{	
+	/**
+	@brief Identifies an image from disk, given its file name
+	@param lpszPathName Path and file name of the image to identify.
+	@return Returns the found FreeImage format if successful, returns FIF_UNKNOWN otherwise.
+	@see FreeImage_GetFileType, FreeImage_GetFIFFromFilename, FreeImage documentation
+	*/
+	static FREE_IMAGE_FORMAT identifyFIF(const char* lpszPathName);
+
+	/**
+	UNICODE version of identifyFIF (this function only works under WIN32 and does nothing on other OS)
+	@see FreeImage_GetFileTypeU, FreeImage_GetFIFFromFilenameU, FreeImage documentation
+	*/
+	static FREE_IMAGE_FORMAT identifyFIFU(const wchar_t* lpszPathName);
+
+	/**
+	@brief Identifies an image using the specified FreeImageIO struct and fi_handle.
+	@param io FreeImageIO structure
+	@param handle FreeImage fi_handle
+	@return Returns the found FreeImage format if successful, returns FIF_UNKNOWN otherwise.
+	@see FreeImage_GetFileTypeFromHandle, FreeImage documentation
+	*/
+	static FREE_IMAGE_FORMAT identifyFIFFromHandle(FreeImageIO *io, fi_handle handle);
+
+	/**
+	@brief Identifies an image using the specified memory stream.
+	@param hmem FreeImage memory stream
+	@return Returns the found FreeImage format if successful, returns FIF_UNKNOWN otherwise.
+	@see FreeImage_GetFileTypeFromMemory, FreeImage documentation
+	*/
+	static FREE_IMAGE_FORMAT identifyFIFFromMemory(FIMEMORY *hmem);
 
 	//@}
+
 
 	/** @name Loading & Saving
 	 * Loading and saving is handled by the FreeImage library.
@@ -890,7 +931,7 @@ public:
 	fipWinImage(FREE_IMAGE_TYPE image_type = FIT_BITMAP, WORD width = 0, WORD height = 0, WORD bpp = 0);
 
 	/// Destructor
-	~fipWinImage();
+	virtual ~fipWinImage();
 
 	/// Destroy image data
 	virtual void clear();
@@ -1069,7 +1110,7 @@ public :
 	Free any allocated memory
 	@see FreeImage_CloseMemory
 	*/
-	~fipMemoryIO();
+	virtual ~fipMemoryIO();
 
 	/** Returns TRUE if the internal memory buffer is a valid buffer, returns FALSE otherwise
 	*/
@@ -1178,7 +1219,7 @@ public:
 	Destructor
 	Close the file stream if not already done. 
 	*/
-	~fipMultiPage();
+	virtual ~fipMultiPage();
 
 	/// Returns TRUE if the multi-page stream is opened
 	BOOL isValid() const;
@@ -1311,7 +1352,7 @@ public:
 	Destructor
 	@see FreeImage_DeleteTag
 	*/
-	~fipTag();
+	virtual ~fipTag();
 	/**
 	Construct a FIDT_ASCII tag (ASCII string).<br>
 	This method is useful to store comments or IPTC tags. 
@@ -1489,7 +1530,7 @@ public:
 	Destructor
 	@see FreeImage_FindCloseMetadata
 	*/
-	~fipMetadataFind();
+	virtual ~fipMetadataFind();
 	/**
 	Provides information about the first instance of a tag that matches 
 	the metadata model specified in the <i>model</i> argument. 
