42ace0caea343d8976a377b4491b7daeaff2fcc0
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2009-11-03 13:05:16 +0000

    Removed CRT/MFC dependencies generation from the "LibRawLite" library.

--- trunk/Graphics/LibRawLite/Source/libraw/libraw_types.h
+++ trunk/Graphics/LibRawLite/Source/libraw/libraw_types.h
@@ -23,6 +23,48 @@
 #ifndef _LIBRAW_TYPES_H
 #define _LIBRAW_TYPES_H
 
+#if defined(LIBRAWLITE_LIB_BUILD)
+
+/*
+ * remove automatic CRT/MFC dependencies generation
+ *
+ * see http://www.codeproject.com/KB/cpp/PrivateAssemblyProjects.aspx and
+ * http://blog.m-ri.de/index.php/2008/05/06/hotfix-fuer-usemsprivateassembliesh-und-vc-2008/
+ * by by Martin Richter for more information
+ */
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1400) && !defined(UNDER_CE)
+/* VC++ 8.0 or greater */
+
+#pragma once
+
+#define _STL_NOFORCE_MANIFEST
+#define _CRT_NOFORCE_MANIFEST
+#define _AFX_NOFORCE_MANIFEST
+#define _ATL_NOFORCE_MANIFEST
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+__declspec(selectany) int _forceCRTManifest;
+__declspec(selectany) int _forceMFCManifest;
+__declspec(selectany) int _forceAtlDllManifest;
+
+/* the next symbols are used by the several versions of VC++ 9.0 */
+__declspec(selectany) int _forceCRTManifestRTM;
+__declspec(selectany) int _forceMFCManifestRTM;
+__declspec(selectany) int _forceMFCManifestCUR;
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* _MSC_VER && UNDER_CE */
+
+#endif /* LIBRAWLITE_LIB_BUILD */
+
 #ifndef WIN32
 #include <sys/time.h>
 #endif
