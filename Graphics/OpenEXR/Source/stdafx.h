84e526035ab7d518b72fb670779826b16b2321d8
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2009-11-03 10:18:16 +0000

    Removed CRT/MFC dependencies generation from the "OpenEXR" library.

--- Graphics/OpenEXR/Source/stdafx.h
+++ Graphics/OpenEXR/Source/stdafx.h
@@ -37,6 +37,40 @@
 #ifndef INCLUDED_STDAFX_H
 #define INCLUDED_STDAFX_H
 
+// remove automatic CRT/MFC dependencies generation
+
+// see http://www.codeproject.com/KB/cpp/PrivateAssemblyProjects.aspx and
+// http://blog.m-ri.de/index.php/2008/05/06/hotfix-fuer-usemsprivateassembliesh-und-vc-2008/
+// by by Martin Richter for more information
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1400)
+// VC++ 8.0 or greater
+
+#define _STL_NOFORCE_MANIFEST
+#define _CRT_NOFORCE_MANIFEST
+#define _AFX_NOFORCE_MANIFEST
+#define _ATL_NOFORCE_MANIFEST
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+__declspec(selectany) int _forceCRTManifest;
+__declspec(selectany) int _forceMFCManifest;
+__declspec(selectany) int _forceAtlDllManifest;
+
+// the next symbols are used by the several versions of VC++ 9.0
+__declspec(selectany) int _forceCRTManifestRTM;
+__declspec(selectany) int _forceMFCManifestRTM;
+__declspec(selectany) int _forceMFCManifestCUR;
+
+#ifdef __cplusplus
+}   // extern "C"
+#endif
+
+#endif   // _MSC_VER
+
 #include <assert.h>
 #include <float.h>
 #include <string.h>
