2d1462017f513ecde45fe6c26d77b6cd284b70b3
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-01-09 10:37:27 +0000

    Updated copyright information in the "AfxGadgets" library sources.

--- trunk/AfxGadgets/Source/MemoryDC.cpp
+++ trunk/AfxGadgets/Source/MemoryDC.cpp
@@ -1,5 +1,5 @@
 // AfxGadgets library.
-// Copyright (c) 2004-2009 by Elijah Zarezky,
+// Copyright (c) 2004-2010 by Elijah Zarezky,
 // All rights reserved.
 // Portions copyright (c) 1996-2002 Keith Rule.
 
