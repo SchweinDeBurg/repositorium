ed52b4d7d7f14df67eb09088fd815f5375f7967b
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-07-14 12:16:11 +0000

    Added more generic helper classes from the ToDoList shared codebase.

---
+++ CodeProject/Source/LimitSingleInstance.h
@@ -0,0 +1,70 @@
+// Copyright (C) 2003-2005 AbstractSpoon Software.
+//
+// This license applies to everything in the ToDoList package, except where
+// otherwise noted.
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the
+// use of this software. 
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in a
+// product, an acknowledgment in the product documentation would be appreciated
+// but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+
+// LimitSingleInstance.h: interface for the CLimitSingleInstance class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_LIMITSINGLEINSTANCE_H__14CD9485_CDA3_4146_9AB9_B3CDC0C2568B__INCLUDED_)
+#define AFX_LIMITSINGLEINSTANCE_H__14CD9485_CDA3_4146_9AB9_B3CDC0C2568B__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+#include <windows.h> 
+
+//this code is from Q243953 in case you lose the article and wonder
+//where this code came from...
+class CLimitSingleInstance
+{
+protected:
+	DWORD  m_dwLastError;
+	HANDLE m_hMutex;
+	
+public:
+	CLimitSingleInstance(TCHAR *strMutexName)
+	{
+		//be sure to use a name that is unique for this application otherwise
+		//two apps may think they are the same if they are using same name for
+		//3rd parm to CreateMutex
+		m_hMutex = CreateMutex(NULL, FALSE, strMutexName); //do early
+		m_dwLastError = GetLastError(); //save for use later...
+	}
+	
+	~CLimitSingleInstance() 
+	{
+		if (m_hMutex)  //don't forget to close handles...
+		{
+			CloseHandle(m_hMutex); //do as late as possible
+			m_hMutex = NULL; //good habit to be in
+		}
+	}
+	
+	BOOL IsAnotherInstanceRunning() 
+	{
+		return (ERROR_ALREADY_EXISTS == m_dwLastError);
+	}
+};
+
+#endif // !defined(AFX_LIMITSINGLEINSTANCE_H__14CD9485_CDA3_4146_9AB9_B3CDC0C2568B__INCLUDED_)
