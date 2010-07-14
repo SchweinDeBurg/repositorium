92779b3a5503c492e4925bf1fdccc3448b8da0db
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-07-14 08:40:36 +0000

    Added more generic helper classes from the ToDoList shared codebase.

---
+++ trunk/CodeProject/Source/AutoComboBox.h
@@ -0,0 +1,134 @@
+// Copyright (C) 2003-2005 AbstractSpoon Software.
+//
+// This license applies to everything in the ToDoList package, except where
+// otherwise noted.
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the
+// use of this software. 
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in a
+// product, an acknowledgment in the product documentation would be appreciated
+// but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+
+#if !defined(AFX_AUTOCOMBOBOX_H__34DD95FF_CCAE_4D8E_9162_D860B65CD448__INCLUDED_)
+#define AFX_AUTOCOMBOBOX_H__34DD95FF_CCAE_4D8E_9162_D860B65CD448__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// AutoComboBox.h : header file
+//
+
+#include "Subclass.h"
+#include "Misc.h"
+
+const UINT WM_ACB_ITEMADDED = ::RegisterWindowMessage(_T("WM_ACB_ITEMADDED"));
+const UINT WM_ACB_ITEMDELETED = ::RegisterWindowMessage(_T("WM_ACB_ITEMDELETED"));
+
+enum 
+{
+	ACBS_ALLOWDELETE	= 0x01,
+	ACBS_CASESENSITIVE	= 0x02,
+	ACBS_ADDTOSTART		= 0x04,
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// CAutoComboBox window
+
+// replacement DDX routine
+void AFXAPI DDX_AutoCBString(CDataExchange* pDX, int nIDC, CString& value);
+
+class CAutoComboBox : public CComboBox, protected CSubclassWnd
+{
+	// Construction
+public:
+	CAutoComboBox(DWORD dwFlags = 0);
+	virtual ~CAutoComboBox();
+	
+	// Operations
+public:
+    virtual int AddString(LPCTSTR szItem) { return AddUniqueItem(szItem); }
+    virtual int InsertString(int nIndex, LPCTSTR szItem) { return InsertUniqueItem(nIndex, szItem); }
+
+	virtual int DeleteString(UINT nIndex) { return CComboBox::DeleteString(nIndex);	}
+	virtual int DeleteString(LPCTSTR szItem, BOOL bCaseSensitive = FALSE);
+
+	virtual int DeleteItems(const CStringArray& aItems, BOOL bCaseSensitive = FALSE); // returns deleted count
+	
+	virtual int AddUniqueItem(const CString& sItem); // returns index or CB_ERR
+    virtual int InsertUniqueItem(int nIndex, const CString& sItem); // returns index or CB_ERR
+	
+    virtual int AddUniqueItems(const CStringArray& aItems); // returns num items added
+    virtual int AddUniqueItems(const CAutoComboBox& cbItems); // returns num items added
+	
+    virtual int GetItems(CStringArray& aItems) const; // returns item count
+    virtual int SelectString(int nStartAfter, LPCTSTR lpszString) { return CComboBox::SelectString(nStartAfter, lpszString); }
+	
+    // helpers
+    int FindStringExact(int nIndexStart, const CString& sItem, BOOL bCaseSensitive) const;
+    int FindStringExact(int nIndexStart, LPCTSTR lpszFind) const
+		{ return FindStringExact(nIndexStart, lpszFind, FALSE); }
+
+	void Flush() { if (m_bEditChange) HandleReturnKey(); }
+	
+protected:
+	DWORD m_dwFlags;
+	HWND m_hwndListbox;
+	BOOL m_bEditChange;
+	
+	// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CAutoComboBox)
+protected:
+	//}}AFX_VIRTUAL
+	virtual LRESULT WindowProc(HWND hRealWnd, UINT msg, WPARAM wp, LPARAM lp);
+	
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CAutoComboBox)
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor);
+	afx_msg void OnKillFocus(CWnd* pNewWnd);
+	//}}AFX_MSG
+	afx_msg BOOL OnSelEndOK();
+	afx_msg BOOL OnSelChange();
+	afx_msg BOOL OnDropDown();
+	afx_msg BOOL OnCloseUp();
+	afx_msg BOOL OnEditChange();
+//	afx_msg BOOL OnKillFocus();
+	
+	DECLARE_MESSAGE_MAP()
+		
+protected:
+	virtual BOOL DeleteSelectedLBItem();
+	virtual void RefreshMaxDropWidth();
+	
+	BOOL AllowDelete() const { return Misc::HasFlag(m_dwFlags, ACBS_ALLOWDELETE); }
+	BOOL CaseSensitive() const { return Misc::HasFlag(m_dwFlags, ACBS_CASESENSITIVE); }
+	BOOL AddToStart() const { return Misc::HasFlag(m_dwFlags, ACBS_ADDTOSTART); }
+	int AddUniqueItem(const CString& sItem, BOOL bAddToStart);
+	
+	BOOL IsSimpleCombo();
+	virtual void HandleReturnKey();
+	void NotifyParent(UINT nIDNotify);
+	virtual CString GetSelectedItem() const;
+
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_AUTOCOMBOBOX_H__34DD95FF_CCAE_4D8E_9162_D860B65CD448__INCLUDED_)
---
+++ trunk/CodeProject/Source/DockManager.h
@@ -0,0 +1,97 @@
+// Copyright (C) 2003-2005 AbstractSpoon Software.
+//
+// This license applies to everything in the ToDoList package, except where
+// otherwise noted.
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the
+// use of this software. 
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in a
+// product, an acknowledgment in the product documentation would be appreciated
+// but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+
+// DockManager.h: interface for the CDockManager class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_DOCKMANAGER_H__E0312E72_217B_4520_88F2_6708F4148F9D__INCLUDED_)
+#define AFX_DOCKMANAGER_H__E0312E72_217B_4520_88F2_6708F4148F9D__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "Subclass.h"
+
+enum DM_POS
+{
+	DMP_UNDOCKED = -1,
+	DMP_LEFT,
+	DMP_RIGHT,
+	DMP_BELOW,
+};
+
+class CDockManager : public CSubclassWnd, public CSubclasser  
+{
+public:
+	CDockManager();
+	virtual ~CDockManager();
+
+	BOOL Initialize(CWnd* pMainWnd, CWnd* pDockWnd, 
+					DM_POS nPos = DMP_UNDOCKED,	DM_POS nLastPos = DMP_RIGHT,
+					int nWidthDocked = -1, int nWidthDockedMax = -1, 
+					int nHeightDocked = -1, int nHeightDockedMax = -1);
+	BOOL Initialized() { return IsHooked(); }
+	BOOL Dock(DM_POS nPos);
+	BOOL UnDock();
+
+	inline DM_POS GetDockPos() { return m_nDockPos; }
+	inline DM_POS GetLastDockPos() { return IsDocked() ? m_nDockPos : m_nLastDockPos; }
+	inline BOOL IsDocked() { return m_nDockPos != DMP_UNDOCKED; }
+	CRect GetUnDockedRect();
+	int GetDockedWidth(BOOL bMaximized) { return bMaximized ? m_nWidthDockedMax : m_nWidthDocked; }
+	int GetDockedHeight(BOOL bMaximized) { return bMaximized ? m_nHeightDockedMax : m_nHeightDocked; }
+
+   inline BOOL Maximized() { return ::IsZoomed(GetHwnd()); }
+
+protected:
+	DM_POS m_nDockPos, m_nLastDockPos;
+	CRect m_rUndocked;
+	int m_nWidthDocked, m_nWidthDockedMax;
+	int m_nHeightDocked, m_nHeightDockedMax;
+	CSize m_sizeMainMin, m_sizeDockMin;
+	BOOL m_bResizeUpdate;
+	BOOL m_bSizeUpdate;
+
+protected:
+	virtual LRESULT WindowProc(HWND hRealWnd, UINT msg, WPARAM wp, LPARAM lp); // main wnd
+	virtual LRESULT ScWindowProc(HWND hRealWnd, UINT msg, WPARAM wp, LPARAM lp); // dock wnd
+	virtual CSubclasser* GetTopSubclasser() { return this; }
+
+	void UpdateDockWindowPos();
+	void UpdateMainWindowPos();
+	
+	CRect GetWorkArea(BOOL bMaximized = TRUE);
+	void MoveWindow(CWnd* pWnd, CRect rect);
+	CSize GetMinMaximizedSize();
+	void FitDockWindowToWorkArea();
+	
+	// pseudo message handlers
+	void OnMaximize();
+	void OnRestore();
+	void OnMinMaxInfo(LPMINMAXINFO pMMI, BOOL bMain);
+
+};
+
+#endif // !defined(AFX_DOCKMANAGER_H__E0312E72_217B_4520_88F2_6708F4148F9D__INCLUDED_)
