a5c92bde5e468a0a40f0a5d5f510b6b576e66141
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-07-14 02:36:21 +0000

    Added more generic helper classes from the ToDoList shared codebase.

---
+++ CodeProject/Source/EnCommandLineInfo.h
@@ -0,0 +1,41 @@
+// EnCommandLineInfo.h: interface for the CEnCommandLineInfo class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_ENCOMMANDLINEINFO_H__71F4DB4E_3B2B_4F6A_B80F_0BB288E68335__INCLUDED_)
+#define AFX_ENCOMMANDLINEINFO_H__71F4DB4E_3B2B_4F6A_B80F_0BB288E68335__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+
+class CEnCommandLineInfo : public CCommandLineInfo  
+{
+public:
+	CEnCommandLineInfo();
+	virtual ~CEnCommandLineInfo();
+
+	BOOL HasOption(LPCTSTR szFlag) const; 
+	BOOL GetOption(LPCTSTR szFlag, CStringArray& aParams) const; 
+	BOOL GetOption(LPCTSTR szFlag, CString& sParam) const; 
+	CString GetOption(LPCTSTR szFlag) const; 
+
+	void DeleteOption(LPCTSTR szFlag);
+
+	BOOL SetOption(LPCTSTR szFlag, LPCTSTR szParam); 
+	BOOL SetOption(LPCTSTR szFlag, DWORD dwParam); 
+
+	static CString ResolveShortcut(LPCTSTR szShortcut);
+
+protected:
+	CMapStringToString m_mapCommandLine;
+	CString m_sLastOption;
+	int m_nLastParameter;
+
+protected:
+	virtual void ParseParam(LPCTSTR lpszParam, BOOL bFlag, BOOL bLast);
+
+
+};
+
+#endif // !defined(AFX_ENCOMMANDLINEINFO_H__71F4DB4E_3B2B_4F6A_B80F_0BB288E68335__INCLUDED_)
---
+++ CodeProject/Source/TabCtrlEx.h
@@ -0,0 +1,74 @@
+#if !defined(AFX_TABCTRLEX_H__F97F9C15_5623_4A5E_ACB5_C10D4C8EB75D__INCLUDED_)
+#define AFX_TABCTRLEX_H__F97F9C15_5623_4A5E_ACB5_C10D4C8EB75D__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// TabCtrlEx.h : header file
+//
+
+#include "XPTabCtrl.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CTabCtrlEx window
+
+// flag
+enum
+{
+	TCE_POSTDRAW		= 0x0001,
+	TCE_MBUTTONCLICK	= 0x0002,
+
+	TCE_ALL				= 0xffff
+};
+
+#ifndef NM_MCLICK
+#	define NM_MCLICK (NM_LAST+1)
+#endif
+
+typedef struct tagTCMBTNCLK
+{
+    NMHDR hdr;
+    int iTab;
+    UINT flags;
+} NMTCMBTNCLK;
+
+class CTabCtrlEx : public CXPTabCtrl
+{
+// Construction
+public:
+	CTabCtrlEx(DWORD dwFlags = TCE_ALL);
+
+// Attributes
+protected:
+	DWORD m_dwFlags;
+	BOOL m_bMBtnDown;
+	CPoint m_ptMBtnDown;
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CTabCtrlEx)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CTabCtrlEx();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CTabCtrlEx)
+	afx_msg void OnPaint();
+	afx_msg void OnMButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnMButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnCaptureChanged(CWnd *pWnd);
+	//}}AFX_MSG
+	virtual void DrawItem(LPDRAWITEMSTRUCT lpDrawItemStruct);
+
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_TABCTRLEX_H__F97F9C15_5623_4A5E_ACB5_C10D4C8EB75D__INCLUDED_)
