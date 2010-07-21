725226a21fad09569a5c851aa501d0e5ed6f1acb
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-07-21 09:26:55 +0000

    Continued integration of the ToDoList tool by AbstractSpoon Software.

---
+++ AbstractSpoon/GPExport/Source/GPImporter.h
@@ -0,0 +1,46 @@
+// GPImporter.h: interface for the CGPImporter class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_GPIMPORTER_H__7B3A9934_69F0_46D5_88B4_C84D715772FC__INCLUDED_)
+#define AFX_GPIMPORTER_H__7B3A9934_69F0_46D5_88B4_C84D715772FC__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "../../Common/IImportExport.h"
+#include "../../Common/ITasklist.h"
+
+#include <afxtempl.h>
+
+class CXmlItem;
+
+class CGPImporter : public IImportTasklist    
+{
+public:
+	CGPImporter();
+	virtual ~CGPImporter();
+
+	// interface implementation
+	void Release() { delete this; }
+
+	// caller must copy only
+	const char* GetMenuText() { return "GanttProject"; }
+	const char* GetFileFilter() { return "GanttProject Files (*.gan)|*.gan||"; }
+	const char* GetFileExtension() { return "gan"; }
+
+	bool Import(const char* szSrcFilePath, ITaskList* pDestTaskFile);
+
+protected:
+	CMap<int, int, CString, CString&> m_mapResources;
+	CMap<int, int, HTASKITEM, HTASKITEM&> m_mapTasks;
+
+protected:
+	bool ImportTask(const CXmlItem* pXISrcTask, ITaskList7* pDestTaskFile, HTASKITEM htDestParent);
+	void BuildResourceMap(const CXmlItem* pXISrcPrj);
+//	void FixupDependencies(ITaskList7* pDestTaskFile, HTASKITEM htTask);
+	void FixupResourceAllocations(const CXmlItem* pXISrcPrj, ITaskList7* pDestTaskFile);
+};
+
+#endif // !defined(AFX_GPIMPORTER_H__7B3A9934_69F0_46D5_88B4_C84D715772FC__INCLUDED_)
---
+++ AbstractSpoon/GPExport/Source/StdAfx.cpp
@@ -0,0 +1,23 @@
+// StdAfx.cpp : source file that includes just the standard includes
+//	GPExport.pch will be the pre-compiled header
+//	StdAfx.obj will contain the pre-compiled type information
+
+#include "StdAfx.h"
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// manifest dependencies
+
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+// VC-2008 RTM                       9.0.21022.8
+// VC-2008 Feature Pack              9.0.30411.0
+// VC-2008 SP1                       9.0.30729.1
+// VC-2008 SP1 ATL Security Update   9.0.30729.4148
+
+#if defined(_DEBUG)
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#else
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateMFC' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#endif   // _DEBUG
