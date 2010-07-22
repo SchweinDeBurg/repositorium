de9be2f8923f5ee54205a37548e024f63e731910
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-07-22 13:26:37 +0000

    Reformatted newly added RTFContentCtrl's source code.

--- AbstractSpoon/RTFContentCtrl/Source/Util.cpp
+++ AbstractSpoon/RTFContentCtrl/Source/Util.cpp
@@ -12,41 +12,41 @@ static char THIS_FILE[]=__FILE__;
 CString LongToString(long newValue)
 {
 
-   long lvalue;
-   CString str_rtc;
-   lvalue = newValue;
-   str_rtc.Format("%d",lvalue);
-   return str_rtc;
+	long lvalue;
+	CString str_rtc;
+	lvalue = newValue;
+	str_rtc.Format("%d",lvalue);
+	return str_rtc;
 
 } //LongToString()
 
 long StringToLong(CString newValue)
 {
-   long lvalue;
-   char *p_str;
-   char *p_endstr;
-   CString str;
-   str = newValue;
-   p_str=(LPSTR) LPCTSTR(str);
-   lvalue = strtol(p_str, &p_endstr, 10);
+	long lvalue;
+	char *p_str;
+	char *p_endstr;
+	CString str;
+	str = newValue;
+	p_str=(LPSTR) LPCTSTR(str);
+	lvalue = strtol(p_str, &p_endstr, 10);
 
-   return lvalue;
+	return lvalue;
 
 } //StringToLong()
 
 void StringToArray(const CString& strSrc, CStringArray& trgt) {
-   CString strDelimiter=";";
-   CString strElement;
-   long lPosition=0;
+	CString strDelimiter=";";
+	CString strElement;
+	long lPosition=0;
 
-   while (lPosition<strSrc.GetLength()) {
-      if ((strDelimiter.Find(strSrc.GetAt(lPosition))>=0)) {
-         trgt.Add(strElement);
-         strElement = "";
-      } else {
-         strElement += strSrc.GetAt(lPosition);
-      } //if-else is_field_delimiter
-      lPosition++;
-   }
-   trgt.Add(strElement); // add last
+	while (lPosition<strSrc.GetLength()) {
+		if ((strDelimiter.Find(strSrc.GetAt(lPosition))>=0)) {
+			trgt.Add(strElement);
+			strElement = "";
+		} else {
+			strElement += strSrc.GetAt(lPosition);
+		} //if-else is_field_delimiter
+		lPosition++;
+	}
+	trgt.Add(strElement); // add last
 }
