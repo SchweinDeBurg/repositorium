82207479f1d79966db96a8404ca8336781a6c74c
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-07-22 09:16:01 +0000

    Added copyright information to the AbstractSpoon sources.

--- trunk/AbstractSpoon/EncryptDecrypt/Source/EncryptDecrypt.cpp
+++ trunk/AbstractSpoon/EncryptDecrypt/Source/EncryptDecrypt.cpp
@@ -1,3 +1,12 @@
+// Copyright by Richard Butler.
+
+//*****************************************************************************
+// Modified by Elijah Zarezky aka SchweinDeBurg (elijah.zarezky@gmail.com):
+// - improved compatibility with the Unicode-based builds
+// - added AbstractSpoon Software copyright notice and licenese information
+// - adjusted #include's paths
+//*****************************************************************************
+
 // EncryptDecrypt.cpp : Defines the entry point for the DLL application.
 //
 
--- trunk/AbstractSpoon/EncryptDecrypt/Source/Encryption.h
+++ trunk/AbstractSpoon/EncryptDecrypt/Source/Encryption.h
@@ -1,3 +1,12 @@
+// Copyright by Richard Butler.
+
+//*****************************************************************************
+// Modified by Elijah Zarezky aka SchweinDeBurg (elijah.zarezky@gmail.com):
+// - improved compatibility with the Unicode-based builds
+// - added AbstractSpoon Software copyright notice and licenese information
+// - adjusted #include's paths
+//*****************************************************************************
+
 // Encryption.h: interface for the CEncryption class.
 //
 //////////////////////////////////////////////////////////////////////
