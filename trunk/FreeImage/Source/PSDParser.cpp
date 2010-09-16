c0868ae4201aa697d96fdf1b5f58f918c10202ad
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-09-16 14:39:01 +0000

    Merged "FreeImage" library with SourceForge VCS: ! FreeImage now uses LibRaw 0.11-Beta2 * [Herve Drolon] fixed a crash in PSD plugin when loading a PSD with a CMYK embedded thumbnail

--- trunk/FreeImage/Source/PSDParser.cpp
+++ trunk/FreeImage/Source/PSDParser.cpp
@@ -312,8 +312,8 @@ psdThumbnail::~psdThumbnail() {
 	SAFE_DELETE_ARRAY(_plData); 
 }
 
-int psdThumbnail::Read(FreeImageIO *io, fi_handle handle, int iTotalData, bool isBGR) {
-	BYTE c[1], ShortValue[2], IntValue[4];
+int psdThumbnail::Read(FreeImageIO *io, fi_handle handle, int iResourceSize, bool isBGR) {
+	BYTE ShortValue[2], IntValue[4];
 	int nBytes=0, n;
 	
 	n = (int)io->read_proc(&IntValue, sizeof(IntValue), 1, handle);
@@ -348,6 +348,20 @@ int psdThumbnail::Read(FreeImageIO *io, fi_handle handle, int iTotalData, bool i
 	nBytes += n * sizeof(ShortValue);
 	_Planes = (short)psdGetValue(ShortValue, sizeof(_Planes) );
 
+	// remove the header size (28 bytes) from the total data size
+	int iTotalData = iResourceSize - nBytes;
+
+	// skip the thumbnail part
+	io->seek_proc(handle, iTotalData, SEEK_CUR);
+
+	return iResourceSize;
+
+	/**
+	TO BE IMPLEMENTED
+	*/
+	/*
+	BYTE c[1];
+
 	_plData = new BYTE[iTotalData];
 	  
 	if (isBGR) {
@@ -375,6 +389,7 @@ int psdThumbnail::Read(FreeImageIO *io, fi_handle handle, int iTotalData, bool i
 	}
 
 	return nBytes;
+	*/
 }
 
 //---------------------------------------------------------------------------
