262eb864e2b7e3abbe5ffa5f0021c2a032edd5b7
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-11-19 08:36:06 +0000

    Updated "LibRaw" library to version 0.11.2 (2010-11-11).

--- trunk/Graphics/LibRawLite/Source/libraw/libraw_types.h
+++ trunk/Graphics/LibRawLite/Source/libraw/libraw_types.h
@@ -68,11 +68,18 @@ __declspec(selectany) int _forceMFCManifestCUR;
 #include <sys/time.h>
 #endif
 #include <stdio.h>
-#include <time.h>
-#ifdef _OPENMP
-#ifdef _MSC_VER
-#error OpenMP is not supported under MS Visual Studio
+
+#if defined (_OPENMP) 
+# if defined(_MSC_VER)
+#  undef LIBRAW_USE_OPENMP
+# elif (defined(__APPLE__) || defined(__MACOSX__)) && defined(_REENTRANT)
+#   undef LIBRAW_USE_OPENMP
+# else /* OpenMP defined but not Mac/pthreads and not Windows */
+#   define LIBRAW_USE_OPENMP
+# endif
 #endif
+
+#ifdef LIBRAW_USE_OPENMP
 #include <omp.h>
 #endif
 
