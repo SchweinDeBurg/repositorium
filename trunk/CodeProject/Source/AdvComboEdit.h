06f06c3e9cab5815c0092eb1b28bfa09efa92d39
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-09 05:24:18 +0000

    Added copyright notice to the CAdvComboEdit sources in the "CodeProject" library.

--- trunk/CodeProject/Source/AdvComboEdit.h
+++ trunk/CodeProject/Source/AdvComboEdit.h
@@ -1,5 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////
 // AdvComboEdit.h : header file
 //
+// CAdvComboEdit Control
+// Version: 2.1
+// Date: September 2002
+// Author: Mathias Tunared
+// Email: Mathias@inorbit.com
+// Copyright (c) 2002. All Rights Reserved.
+//
+// This code, in compiled form or as source code, may be redistributed
+// unmodified PROVIDING it is not sold for profit without the authors
+// written consent, and providing that this notice and the authors name
+// and all copyright notices remains intact.
+//
+// This file is provided "as is" with no expressed or implied warranty.
+// The author accepts no liability for any damage/loss of business that
+// this product may cause.
+//
+/////////////////////////////////////////////////////////////////////////////
 
 #ifndef ADVCOMBOEDIT_H
 #define ADVCOMBOEDIT_H
