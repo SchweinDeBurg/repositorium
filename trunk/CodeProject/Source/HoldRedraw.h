836ebf23f6d4897fe01b2c5bafaea94cf47a4224
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-09 09:39:13 +0000

    Reformatted CHoldRedraw sources in the "CodeProject" library.

--- trunk/CodeProject/Source/HoldRedraw.h
+++ trunk/CodeProject/Source/HoldRedraw.h
@@ -5,7 +5,7 @@
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the
-// use of this software. 
+// use of this software.
 //
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute it
@@ -26,6 +26,19 @@
 // - improved compatibility with the Unicode-based builds
 // - added AbstractSpoon Software copyright notice and licenese information
 // - taken out from the original ToDoList package for better sharing
+// - reformatted with using Artistic Style 2.01 and the following options:
+//      --indent=tab=3
+//      --indent=force-tab=3
+//      --indent-switches
+//      --max-instatement-indent=2
+//      --brackets=break
+//      --add-brackets
+//      --pad-oper
+//      --unpad-paren
+//      --pad-header
+//      --align-pointer=type
+//      --lineend=windows
+//      --suffix=none
 //*****************************************************************************
 
 // HoldRedraw.h: interface and implementation of the CHoldRedraw class.
@@ -77,14 +90,18 @@ public:
 			TRACE(_T("~CRedrawAll()\n"));
 
 			if (m_dwFlags & NCR_NCPAINT)
+			{
 				::SendMessage(m_hWnd, WM_NCPAINT, 0, 0);
+			}
 
 			if (m_dwFlags & NCR_PAINT)
 			{
 				::InvalidateRect(m_hWnd, NULL, FALSE);
 
 				if (m_dwFlags & NCR_UPDATE)
+				{
 					::UpdateWindow(m_hWnd);
+				}
 			}
 		}
 	}
@@ -101,7 +118,7 @@ public:
 	{
 		if (m_hWnd)
 		{
-			TRACE (_T("Disabling redraw on 0x%08x\n"), m_hWnd);
+			TRACE(_T("Disabling redraw on 0x%08x\n"), m_hWnd);
 			::SendMessage(m_hWnd, WM_SETREDRAW, FALSE, 0);
 		}
 	}
@@ -110,7 +127,7 @@ public:
 	{
 		if (m_hWnd)
 		{
-			TRACE (_T("Re-enabling redraw on 0x%08x\n"), m_hWnd);
+			TRACE(_T("Re-enabling redraw on 0x%08x\n"), m_hWnd);
 			::SendMessage(m_hWnd, WM_SETREDRAW, TRUE, 0);
 		}
 	}
