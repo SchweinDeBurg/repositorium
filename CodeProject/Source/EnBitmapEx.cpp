28634c70b2407b9d4b3606b9a09832e4b080548c
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-09 07:45:47 +0000

    Reformatted CEnBitmapEx sources in the "CodeProject" library.

--- CodeProject/Source/EnBitmapEx.cpp
+++ CodeProject/Source/EnBitmapEx.cpp
@@ -1,9 +1,51 @@
+// Copyright (C) 2003-2005 AbstractSpoon Software.
+//
+// This license applies to everything in the ToDoList package, except where
+// otherwise noted.
+//
+// This software is provided 'as-is', without any express or implied warranty.
+// In no event will the authors be held liable for any damages arising from the
+// use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+// claim that you wrote the original software. If you use this software in a
+// product, an acknowledgment in the product documentation would be appreciated
+// but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+// misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+
+//*****************************************************************************
+// Modified by Elijah Zarezky aka SchweinDeBurg (elijah.zarezky@gmail.com):
+// - added AbstractSpoon Software copyright notice and licenese information
+// - taken out from the original ToDoList package for better sharing
+// - reformatted with using Artistic Style 2.01 and the following options:
+//      --indent=tab=3
+//      --indent=force-tab=3
+//      --indent-switches
+//      --max-instatement-indent=2
+//      --brackets=break
+//      --add-brackets
+//      --pad-oper
+//      --unpad-paren
+//      --pad-header
+//      --align-pointer=type
+//      --lineend=windows
+//      --suffix=none
+//*****************************************************************************
+
 // EnBitmap.cpp: implementation of the CEnBitmapEx class (c) daniel godson 2002.
 //
-// credits: Peter Hendrix's CPicture implementation for the original IPicture code 
-//          Yves Maurer's GDIRotate implementation for the idea of working directly on 32 bit representations of bitmaps 
-//          Karl Lager's 'A Fast Algorithm for Rotating Bitmaps' 
-// 
+// credits: Peter Hendrix's CPicture implementation for the original IPicture code
+//          Yves Maurer's GDIRotate implementation for the idea of working directly on 32 bit representations of bitmaps
+//          Karl Lager's 'A Fast Algorithm for Rotating Bitmaps'
+//
 //////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
@@ -12,7 +54,7 @@
 
 #ifdef _DEBUG
 #undef THIS_FILE
-static char THIS_FILE[]=__FILE__;
+static char THIS_FILE[] = __FILE__;
 #define new DEBUG_NEW
 #endif
 
@@ -31,7 +73,9 @@ CEnBitmapEx::~CEnBitmapEx()
 BOOL CEnBitmapEx::LoadSysBitmap(UINT nID)
 {
 	if (LoadBitmap(nID))
+	{
 		return RemapSysColors();
+	}
 
 	return FALSE;
 }
