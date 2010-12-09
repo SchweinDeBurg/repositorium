ef656710d6e059746f34ae0472d797bc1821ac9e
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-09 05:41:30 +0000

    Reformatted CSortCStringArray sources in the "CodeProject" library.

--- trunk/CodeProject/Source/SortCStringArray.h
+++ trunk/CodeProject/Source/SortCStringArray.h
@@ -16,59 +16,69 @@ private:
 	~CSortCStringArray() {};
 
 public:
-	static void SortCStringArray(CStringArray * psa, 
-								 BOOL bCaseSensitive = FALSE, 
-								 BOOL bAscending = TRUE)
+	static void SortCStringArray(CStringArray* psa,
+		BOOL bCaseSensitive = FALSE,
+		BOOL bAscending = TRUE)
 	{
 		int nArraySize = psa->GetSize();
 		if (nArraySize <= 0)
+		{
 			return;
+		}
 
-		int nCStringSize = sizeof (CString*);
-		void* pArrayStart = (void *)&(*psa)[0];
+		int nCStringSize = sizeof(CString*);
+		void* pArrayStart = (void*) & (*psa)[0];
 
 		if (bCaseSensitive)
 		{
 			if (bAscending)
+			{
 				qsort(pArrayStart, nArraySize, nCStringSize, CompareAscending);
+			}
 			else
+			{
 				qsort(pArrayStart, nArraySize, nCStringSize, CompareDescending);
+			}
 		}
 		else
 		{
 			if (bAscending)
+			{
 				qsort(pArrayStart, nArraySize, nCStringSize, CompareNCAscending);
+			}
 			else
+			{
 				qsort(pArrayStart, nArraySize, nCStringSize, CompareNCDescending);
+			}
 		}
 	}
 
 private:
-	static int CompareAscending(const void *a, const void *b)
+	static int CompareAscending(const void* a, const void* b)
 	{
-		CString *pA = (CString*)a;
-		CString *pB = (CString*)b;
+		CString* pA = (CString*)a;
+		CString* pB = (CString*)b;
 		return (pA->Compare(*pB));
 	}
 
-	static int CompareDescending(const void *a, const void *b)
+	static int CompareDescending(const void* a, const void* b)
 	{
-		CString *pA = (CString*)a;
-		CString *pB = (CString*)b;
+		CString* pA = (CString*)a;
+		CString* pB = (CString*)b;
 		return (-1 * (pA->Compare(*pB)));
 	}
 
-	static int CompareNCAscending(const void *a, const void *b)
+	static int CompareNCAscending(const void* a, const void* b)
 	{
-		CString *pA = (CString*)a;
-		CString *pB = (CString*)b;
+		CString* pA = (CString*)a;
+		CString* pB = (CString*)b;
 		return (pA->CompareNoCase(*pB));
 	}
 
-	static int CompareNCDescending(const void *a, const void *b)
+	static int CompareNCDescending(const void* a, const void* b)
 	{
-		CString *pA = (CString*)a;
-		CString *pB = (CString*)b;
+		CString* pA = (CString*)a;
+		CString* pB = (CString*)b;
 		return (-1 * (pA->CompareNoCase(*pB)));
 	}
 };
