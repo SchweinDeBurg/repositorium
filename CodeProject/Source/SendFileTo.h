9eed9e5dba0cdd191807ac2de5ee7dd104827a65
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-10 06:54:58 +0000

    Reformatted CSendFileTo sources in the "CodeProject" library.

--- CodeProject/Source/SendFileTo.h
+++ CodeProject/Source/SendFileTo.h
@@ -4,31 +4,37 @@
 #include <mapi.h>
 
 #ifndef ULONG_PTR
-#define ULONG_PTR ULONG 
+#define ULONG_PTR ULONG
 #endif
 
 class CSendFileTo
 {
 public:
-	static bool SendMail(HWND hWndParent, CString const &strSubject=_T(""), CString const &strBody=_T(""), CString const &strAttachmentFileName=_T(""))
+	static bool SendMail(HWND hWndParent, CString const& strSubject = _T(""), CString const& strBody = _T(""), CString const& strAttachmentFileName = _T(""))
 	{
 		if (!hWndParent || !::IsWindow(hWndParent))
+		{
 			return false;
+		}
 
 		HINSTANCE hMAPI = ::LoadLibrary(_T("MAPI32.DLL"));
 		if (!hMAPI)
+		{
 			return false;
+		}
 
-		typedef ULONG (PASCAL *PFNSENDMAIL)(ULONG, ULONG_PTR, MapiMessage*, FLAGS, ULONG);
+		typedef ULONG(PASCAL * PFNSENDMAIL)(ULONG, ULONG_PTR, MapiMessage*, FLAGS, ULONG);
 		PFNSENDMAIL fnSendMail = (PFNSENDMAIL)GetProcAddress(hMAPI, "MAPISendMail");
 
 		if (!fnSendMail)
+		{
 			return false;
+		}
 
 		ATL::CT2A aPathName(strAttachmentFileName);
 
 		MapiFileDesc fileDesc = { 0 };
-		fileDesc.nPosition = (ULONG)-1;
+		fileDesc.nPosition = (ULONG) - 1;
 		fileDesc.lpszPathName = aPathName;
 		fileDesc.lpszFileName = aPathName;
 
@@ -41,10 +47,12 @@ public:
 		message.nFileCount = strAttachmentFileName.IsEmpty() ? 0 : 1;
 		message.lpFiles = strAttachmentFileName.IsEmpty() ? NULL : &fileDesc;
 
-		int nError = fnSendMail(0, (ULONG_PTR)hWndParent, &message, MAPI_LOGON_UI|MAPI_DIALOG, 0);
+		int nError = fnSendMail(0, (ULONG_PTR)hWndParent, &message, MAPI_LOGON_UI | MAPI_DIALOG, 0);
 
 		if (nError != SUCCESS_SUCCESS && nError != MAPI_USER_ABORT && nError != MAPI_E_LOGIN_FAILURE)
+		{
 			return false;
+		}
 
 		return true;
 	}
