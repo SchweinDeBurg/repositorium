386108107ed29aa66ffc69e0974684a6d8f5a2a6
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-10 07:00:26 +0000

    Reformatted CSizeComboBox sources in the "CodeProject" library.

--- CodeProject/Source/SizeComboBox.cpp
+++ CodeProject/Source/SizeComboBox.cpp
@@ -1,22 +1,22 @@
 /* ==========================================================================
-	File :			SizeComboBox.cpp
+    File :          SizeComboBox.cpp
 
-	Class :			CSizeComboBox
+    Class :         CSizeComboBox
 
-	Author :		Johan Rosengren, Abstrakt Mekanik AB
-					Iain Clarke
+    Author :        Johan Rosengren, Abstrakt Mekanik AB
+                    Iain Clarke
 
-	Date :			2005-05-06
+    Date :          2005-05-06
 
-	Purpose :		"CSizeComboBox" is derived from "CComboBox" and is a simple 
-					combobox for displaying a selection of font sizes.
+    Purpose :       "CSizeComboBox" is derived from "CComboBox" and is a simple
+                    combobox for displaying a selection of font sizes.
 
-	Description :	Simpel derived class with members to fill the box and 
-					select an entry by contents instead of index.
+    Description :   Simpel derived class with members to fill the box and
+                    select an entry by contents instead of index.
 
-	Usage :			Create as any combobox, and call "FillCombo" to fill 
-					the control with a selection of font sizes. Call 
-					"SelectFontSize" to select an entry by content.
+    Usage :         Create as any combobox, and call "FillCombo" to fill
+                    the control with a selection of font sizes. Call
+                    "SelectFontSize" to select an entry by content.
 
    ========================================================================*/
 
@@ -34,14 +34,14 @@ static char THIS_FILE[] = __FILE__;
 
 CSizeComboBox::CSizeComboBox()
 /* ============================================================
-	Function :		CSizeComboBox::CSizeComboBox
-	Description :	ctor
-	Access :		Public
-					
-	Return :		void
-	Parameters :	none
+    Function :      CSizeComboBox::CSizeComboBox
+    Description :   ctor
+    Access :        Public
 
-	Usage :			
+    Return :        void
+    Parameters :    none
+
+    Usage :
 
    ============================================================*/
 {
@@ -49,14 +49,14 @@ CSizeComboBox::CSizeComboBox()
 
 CSizeComboBox::~CSizeComboBox()
 /* ============================================================
-	Function :		CSizeComboBox::~CSizeComboBox
-	Description :	dtor
-	Access :		Public
-					
-	Return :		void
-	Parameters :	none
+    Function :      CSizeComboBox::~CSizeComboBox
+    Description :   dtor
+    Access :        Public
 
-	Usage :			
+    Return :        void
+    Parameters :    none
+
+    Usage :
 
    ============================================================*/
 {
@@ -65,69 +65,69 @@ CSizeComboBox::~CSizeComboBox()
 
 BEGIN_MESSAGE_MAP(CSizeComboBox, CComboBox)
 	//{{AFX_MSG_MAP(CSizeComboBox)
-		// NOTE - the ClassWizard will add and remove mapping macros here.
+	// NOTE - the ClassWizard will add and remove mapping macros here.
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // CSizeComboBox message handlers
 
-BOOL CSizeComboBox::SelectSize( int size )
+BOOL CSizeComboBox::SelectSize(int size)
 /* ============================================================
-	Function :		CSizeComboBox::SelectSize
-	Description :	Selects the entry corresponding to "size", 
-					if it exists in the list.
-	Access :		Public
-					
-	Return :		void
-	Parameters :	int size	-	Size to select
+    Function :      CSizeComboBox::SelectSize
+    Description :   Selects the entry corresponding to "size",
+                    if it exists in the list.
+    Access :        Public
 
-	Usage :			Call to select an entry by value in the 
-					combo list.
+    Return :        void
+    Parameters :    int size    -   Size to select
+
+    Usage :         Call to select an entry by value in the
+                    combo list.
 
    ============================================================*/
 {
 	CString sz;
-	sz.Format( _T( "%d" ), size );
+	sz.Format(_T("%d"), size);
 
 	int nFind = FindStringExact(-1, sz);
 
 	if (nFind != GetCurSel())
+	{
 		SetCurSel(nFind);
+	}
 
 	return (nFind != -1);
 }
 
 void CSizeComboBox::FillCombo()
 /* ============================================================
-	Function :		CSizeComboBox::FillCombo
-	Description :	Fills the combo box with a fairly standard 
-					selection of font sizes.
-	Access :		Public
-					
-	Return :		void
-	Parameters :	none
+    Function :      CSizeComboBox::FillCombo
+    Description :   Fills the combo box with a fairly standard
+                    selection of font sizes.
+    Access :        Public
 
-	Usage :			Call to fill the combobox.
+    Return :        void
+    Parameters :    none
+
+    Usage :         Call to fill the combobox.
 
    ============================================================*/
 {
-
-	AddString( _T( "8" ) );
-	AddString( _T( "9" ) );
-	AddString( _T( "10" ) );
-	AddString( _T( "11" ) );
-	AddString( _T( "12" ) );
-	AddString( _T( "14" ) );
-	AddString( _T( "16" ) );
-	AddString( _T( "18" ) );
-	AddString( _T( "20" ) );
-	AddString( _T( "22" ) );
-	AddString( _T( "24" ) );
-	AddString( _T( "26" ) );
-	AddString( _T( "28" ) );
-	AddString( _T( "36" ) );
-	AddString( _T( "48" ) );
-	AddString( _T( "72" ) );
-
+	AddString(_T("8"));
+	AddString(_T("9"));
+	AddString(_T("10"));
+	AddString(_T("11"));
+	AddString(_T("12"));
+	AddString(_T("14"));
+	AddString(_T("16"));
+	AddString(_T("18"));
+	AddString(_T("20"));
+	AddString(_T("22"));
+	AddString(_T("24"));
+	AddString(_T("26"));
+	AddString(_T("28"));
+	AddString(_T("36"));
+	AddString(_T("48"));
+	AddString(_T("72"));
 }
