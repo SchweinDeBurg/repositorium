f7a1acc8c10a41226c81681734622c50e2d7242b
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2010-12-24 05:05:17 +0000

    Updated "LibRawLite" library to version 0.13.0-Alpha2 (2010-12-22).

--- Graphics/LibRawLite/Source/libraw/libraw_types.h
+++ Graphics/LibRawLite/Source/libraw/libraw_types.h
@@ -70,11 +70,11 @@ __declspec(selectany) int _forceMFCManifestCUR;
 #include <stdio.h>
 
 #if defined (_OPENMP) 
-# if defined(_MSC_VER)
+# if (_MSC_VER < 1600) // OpenMP works on VS2010
 #  undef LIBRAW_USE_OPENMP
 # elif (defined(__APPLE__) || defined(__MACOSX__)) && defined(_REENTRANT)
 #   undef LIBRAW_USE_OPENMP
-# else /* OpenMP defined but not Mac/pthreads and not Windows */
+# else 
 #   define LIBRAW_USE_OPENMP
 # endif
 #endif
@@ -308,8 +308,16 @@ typedef struct
     int         eeci_refine;
     int         es_med_passes;
     /* AMaZE*/
-    int         amaze_ca_refine;
-
+    int         ca_correc;
+    float       cared;
+    float	cablue;
+    int cfaline;
+    float linenoise;
+    int cfa_clean;
+    float lclean;
+    float cclean;
+    int cfa_green;
+    float green_thresh;
 
 }libraw_output_params_t;
 
