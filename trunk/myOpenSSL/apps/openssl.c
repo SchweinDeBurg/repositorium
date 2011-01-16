9f6c9106ac27d0d2c1130a9f60655a1145723cae
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-01-16 15:43:13 +0000

    Added x64 make-file to the "OpenSSL" library sources.

--- trunk/myOpenSSL/apps/openssl.c
+++ trunk/myOpenSSL/apps/openssl.c
@@ -110,6 +110,12 @@
  */
 
 
+#if defined(_DEBUG)
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.30729.4148' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#else
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.CRT' version='9.0.21022.8' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#endif   /* _DEBUG */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
