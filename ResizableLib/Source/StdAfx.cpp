29f3e7ccad9ccea9fc0453217f4d1679ad07b5dc
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-02-11 22:27:06 +0000

    Added "ResizableLib" by Paolo Messina.

---
+++ ResizableLib/Source/StdAfx.cpp
@@ -0,0 +1,6 @@
+// StdAfx.cpp : source file that includes just the standard includes
+//	ResizableLib.pch will be the pre-compiled header
+//	StdAfx.obj will contain the pre-compiled type information
+
+#include "StdAfx.h"
+
