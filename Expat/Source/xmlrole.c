690e70bbe0ef11786982fb07906af5f2bc4ea4ae
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-05-11 06:22:29 +0000

    Added "SCEW" library version 1.1.3 (2010/09/14). Improved privatization of the "Expat" library.

--- Expat/Source/xmlrole.c
+++ Expat/Source/xmlrole.c
@@ -2,17 +2,44 @@
    See the file COPYING for copying permission.
 */
 
+#ifdef EXPAT_LIB_BUILD /* we are building the Expat library itself */
+
+/* Prevention of the automatic CRT/MFC dependencies generation.
+
+   See http://www.codeproject.com/KB/cpp/PrivateAssemblyProjects.aspx and
+   http://blog.m-ri.de/index.php/2008/05/06/hotfix-fuer-usemsprivateassembliesh-und-vc-2008/
+   by Martin Richter for more information.
+*/
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1400) && !defined(UNDER_CE)
+/* We are using VC++ 2005 (8.0) or greater. */
+
+#define _CRT_NOFORCE_MANIFEST
+#define _STL_NOFORCE_MANIFEST
+#define _AFX_NOFORCE_MANIFEST
+#define _ATL_NOFORCE_MANIFEST
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
 __declspec(selectany) int _forceCRTManifest;
+__declspec(selectany) int _forceMFCManifest;
+__declspec(selectany) int _forceAtlDllManifest;
+
+/* The next symbols are used by the several versions of VC++ 2008 (9.0). */
 __declspec(selectany) int _forceCRTManifestRTM;
+__declspec(selectany) int _forceMFCManifestRTM;
+__declspec(selectany) int _forceMFCManifestCUR;
 
 #ifdef __cplusplus
-}   /* extern "C" */
+} /* extern "C" */
 #endif
+
+#endif /* _MSC_VER && !UNDER_CE */
+
+#endif /* EXPAT_LIB_BUILD */
 
 #include <stddef.h>
 
