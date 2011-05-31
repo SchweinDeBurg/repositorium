af8499e4c8ac15442ed9b7e80615838e5fc6fd3f
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-05-31 05:46:13 +0000

    Started merging "ToDoList" utility branch with the version 6.2.2 sources.

--- branches/AbstractSpoon/ToDoList-6.2.x/OutlookImpExp/Source/Resource.h
+++ branches/AbstractSpoon/ToDoList-6.2.x/OutlookImpExp/Source/Resource.h
@@ -5,6 +5,7 @@
 #define IDD_IMPORT_DIALOG               2000
 #define IDC_TASKLIST                    2000
 #define IDC_REMOVEOUTLOOKTASKS          2002
+#define IDC_CHOOSEFOLDER                2003
 
 // Next default values for new objects
 // 
@@ -12,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        2001
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         2003
+#define _APS_NEXT_CONTROL_VALUE         2004
 #define _APS_NEXT_SYMED_VALUE           2000
 #endif
 #endif
