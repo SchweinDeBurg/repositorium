5defdd7ce84e00a051fbc4a61b03089dfcb045eb
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-06-01 05:55:04 +0000

    Fixed typo.

--- branches/CodeProject/ToDoList-6.2.x/Source/EnEdit.h
+++ branches/CodeProject/ToDoList-6.2.x/Source/EnEdit.h
@@ -24,7 +24,7 @@
 //*****************************************************************************
 // Modified by Elijah Zarezky aka SchweinDeBurg (elijah.zarezky@gmail.com):
 // - improved compatibility with the Unicode-based builds
-// - added AbstractSpoon Software copyright notice and licenese information
+// - added AbstractSpoon Software copyright notice and license information
 // - taken out from the original ToDoList package for better sharing
 // - reformatted with using Artistic Style 2.01 and the following options:
 //      --indent=tab=3
