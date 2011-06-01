c8373e283b3038b629a4743b5c8e223124f0865f
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-06-01 05:33:58 +0000

    Continued merging "ToDoList" utility branch with the version 6.2.2 sources.

--- branches/AbstractSpoon/ToDoList-6.2.x/ToDoList/Source/TDLSchemaDef.h
+++ branches/AbstractSpoon/ToDoList-6.2.x/ToDoList/Source/TDLSchemaDef.h
@@ -26,6 +26,7 @@
 // - improved compatibility with the Unicode-based builds
 // - added AbstractSpoon Software copyright notice and licenese information
 // - adjusted #include's paths
+// - merged with ToDoList version 6.2.2 sources
 //*****************************************************************************
 
 #if !defined(AFX_TDLSCHEMADEF_5951FDE6_508A_4A9D_A55D_D16EB026AEF7__INCLUDED_)
@@ -52,6 +53,7 @@ static LPCTSTR TDL_FILENAME                  = _T("FILENAME");
 static LPCTSTR TDL_TASK                      = _T("TASK");
 
 static LPCTSTR TDL_TASKID                    = _T("ID");
+static LPCTSTR TDL_TASKPARENTID              = _T("PARENTID");
 static LPCTSTR TDL_TASKLASTMOD               = _T("LASTMOD");
 static LPCTSTR TDL_TASKLASTMODSTRING         = _T("LASTMODSTRING");
 static LPCTSTR TDL_TASKTITLE                 = _T("TITLE");
