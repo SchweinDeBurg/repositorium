ee0956483cc51ff9a88f397fdaf77afea8c3a0cc
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-06-03 04:20:31 +0000

    Adjusted AbstractSpoon's copyright notice in the "ToDoList" utility branch sources.

--- branches/AbstractSpoon/ToDoList-6.2.x/ToDoList/Source/TaskListCsvExporter.h
+++ branches/AbstractSpoon/ToDoList-6.2.x/ToDoList/Source/TaskListCsvExporter.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2003-2005 AbstractSpoon Software.
+// Copyright (C) 2003-2011 AbstractSpoon Software.
 //
 // This license applies to everything in the ToDoList package, except where
 // otherwise noted.
