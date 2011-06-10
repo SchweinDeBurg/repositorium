a65c0fbb54add3bdd0d322e62758c1bece93f608
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-06-10 04:28:59 +0000

    Merged "FreeImage" library with the SourceForge VCS.

--- Graphics/LibRawLite/Source/libraw/libraw_types.h
+++ Graphics/LibRawLite/Source/libraw/libraw_types.h
@@ -81,7 +81,7 @@ __declspec(selectany) int _forceMFCManifestCUR;
 #else
 #  undef LIBRAW_USE_OPENMP
 #endif
-// Not Win32
+/* Not Win32 */
 # elif (defined(__APPLE__) || defined(__MACOSX__)) && defined(_REENTRANT)
 #   undef LIBRAW_USE_OPENMP
 # else 
