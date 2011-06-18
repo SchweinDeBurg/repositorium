c88a63902c2555e7e81fc896ddd973d667be5968
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-06-18 04:31:09 +0000

    Updated manifest dependencies in the isolated "OpenSSL" library.

--- myOpenSSL/ssl/ssl_lib.c
+++ myOpenSSL/ssl/ssl_lib.c
@@ -161,9 +161,9 @@
 #endif
 
 #if defined(_DEBUG)
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.30729.5570' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.DebugCRT' version='9.0.30729.6161' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #else
-#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateCRT' version='9.0.30729.5570' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
+#pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.VC90.PrivateCRT' version='9.0.30729.6161' processorArchitecture='x86' publicKeyToken='1fc8b3b9a1e18e3b'\"")
 #endif   /* _DEBUG */
 
 const char *SSL_version_str=OPENSSL_VERSION_TEXT;
