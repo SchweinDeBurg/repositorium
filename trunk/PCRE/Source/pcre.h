b09f8b212ba679ebcf98588aa23fd173fffa3bfd
Elijah Zarezky <Elijah Zarezky@57b2d01b-9e41-0410-b915-ef3bcffcc944>
2011-12-21 04:12:58 +0000

    Updated "PCRE" library to version 8.21 (2011-12-12) again: news text and release date was changed.

--- trunk/PCRE/Source/pcre.h
+++ trunk/PCRE/Source/pcre.h
@@ -44,7 +44,7 @@ POSSIBILITY OF SUCH DAMAGE.
 #define PCRE_MAJOR          8
 #define PCRE_MINOR          21
 #define PCRE_PRERELEASE
-#define PCRE_DATE           2011-12-05
+#define PCRE_DATE           2011-12-12
 
 /* When an application links to a PCRE DLL in Windows, the symbols that are
 imported have to be identified as such. When building PCRE, the appropriate
